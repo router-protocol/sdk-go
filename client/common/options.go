3a5626164b000c4e230a697a12353e6c6e48f845
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2022-08-16 14:38:15 +0400

    chain client

---
+++ client/common/options.go
@@ -0,0 +1,55 @@
+package common
+
+import (
+	ctypes "github.com/router-protocol/sdk-go/routerchain/types"
+	log "github.com/xlab/suplog"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc/credentials"
+)
+
+func init() {
+	// set the address prefixes
+	config := sdk.GetConfig()
+
+	// This is specific to Router chain
+	ctypes.SetBech32Prefixes(config)
+	ctypes.SetBip44CoinType(config)
+}
+
+type ClientOptions struct {
+	GasPrices string
+	TLSCert   credentials.TransportCredentials
+}
+
+type ClientOption func(opts *ClientOptions) error
+
+func DefaultClientOptions() *ClientOptions {
+	return &ClientOptions{}
+}
+
+func OptionGasPrices(gasPrices string) ClientOption {
+	return func(opts *ClientOptions) error {
+		_, err := sdk.ParseDecCoins(gasPrices)
+		if err != nil {
+			err = errors.Wrapf(err, "failed to ParseDecCoins %s", gasPrices)
+			return err
+		}
+
+		opts.GasPrices = gasPrices
+		return nil
+	}
+}
+
+func OptionTLSCert(tlsCert credentials.TransportCredentials) ClientOption {
+	return func(opts *ClientOptions) error {
+		if tlsCert == nil {
+			log.Infoln("client does not use grpc secure transport")
+		} else {
+			log.Infoln("succesfully load server TLS cert")
+		}
+		opts.TLSCert = tlsCert
+		return nil
+	}
+}
---
+++ routerchain/types/coin.go
@@ -0,0 +1,38 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+const (
+	// ROUTER defines the default coin denomination used in Ethermint in:
+	//
+	// - Staking parameters: denomination used as stake in the dPoS chain
+	// - Mint parameters: denomination minted due to fee distribution rewards
+	// - Governance parameters: denomination used for spam prevention in proposal deposits
+	// - Crisis parameters: constant fee denomination used for spam prevention to check broken invariant
+	// - EVM parameters: denomination used for running EVM state transitions in Ethermint.
+	RouterCoin string = "router"
+
+	// BaseDenomUnit defines the base denomination unit for Photons.
+	// 1 photon = 1x10^{BaseDenomUnit} inj
+	BaseDenomUnit = 18
+)
+
+// NewRouterCoin is a utility function that returns an "inj" coin with the given sdk.Int amount.
+// The function will panic if the provided amount is negative.
+func NewRouterCoin(amount sdk.Int) sdk.Coin {
+	return sdk.NewCoin(RouterCoin, amount)
+}
+
+// NewRouterDecCoin is a utility function that returns an "inj" decimal coin with the given sdk.Int amount.
+// The function will panic if the provided amount is negative.
+func NewRouterDecCoin(amount sdk.Int) sdk.DecCoin {
+	return sdk.NewDecCoin(RouterCoin, amount)
+}
+
+// NewRouterCoinInt64 is a utility function that returns an "inj" coin with the given int64 amount.
+// The function will panic if the provided amount is negative.
+func NewRouterCoinInt64(amount int64) sdk.Coin {
+	return sdk.NewInt64Coin(RouterCoin, amount)
+}
