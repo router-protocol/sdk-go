226acb341ec920e8921c5bed3126f32dcfacbbed
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2022-08-16 14:38:15 +0400

    chain client

---
+++ examples/chain/1_MsgSend/example.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	chainclient "github.com/router-protocol/sdk-go/client/chain"
+	"github.com/router-protocol/sdk-go/client/common"
+
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
+)
+
+func main() {
+	// network := common.LoadNetwork("mainnet", "k8s")
+	network := common.LoadNetwork("testnet", "k8s")
+	tmRPC, err := rpchttp.New(network.TmEndpoint, "/websocket")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	senderAddress, cosmosKeyring, err := chainclient.InitCosmosKeyring(
+		os.Getenv("HOME")+"/.router-chain",
+		"router-chaind",
+		"file",
+		"my-validator",
+		"12345678",
+		"", // keyring will be used if pk not provided
+		false,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// initialize grpc client
+
+	clientCtx, err := chainclient.NewClientContext(
+		network.ChainId,
+		senderAddress.String(),
+		cosmosKeyring,
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	clientCtx = clientCtx.WithNodeURI(network.TmEndpoint).WithClient(tmRPC)
+
+	// prepare tx msg
+
+	msg := &banktypes.MsgSend{
+		FromAddress: senderAddress.String(),
+		ToAddress:   "router1mtp76jwymme78xaf0h73cmky8hdy3thhy0xz9a",
+		Amount: []sdktypes.Coin{{
+			Denom: "stake", Amount: sdktypes.NewInt(1000000000000000000)}, // 1 Stake
+		},
+	}
+
+	chainClient, err := chainclient.NewChainClient(
+		clientCtx,
+		network.ChainGrpcEndpoint,
+		common.OptionTLSCert(network.ChainTlsCert),
+		common.OptionGasPrices("500000000token"),
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//AsyncBroadcastMsg, SyncBroadcastMsg, QueueBroadcastMsg
+	err = chainClient.QueueBroadcastMsg(msg)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	time.Sleep(time.Second * 5)
+
+	gasFee, err := chainClient.GetGasFee()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("gas fee:", gasFee, "INJ")
+}
