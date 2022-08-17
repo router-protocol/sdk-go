9295bc92c58f1bc1cad66eab6c68b3a9aabc2581
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2022-08-17 09:25:47 +0400

    router chain types

--- routerchain/types/chain_id.go
+++ routerchain/types/chain_id.go
@@ -11,10 +11,10 @@ import (
 )
 
 var (
-	regexChainID     = `[a-z]*`
-	regexSeparator   = `-{1}`
-	regexEpoch       = `[1-9][0-9]*`
-	injectiveChainID = regexp.MustCompile(fmt.Sprintf(`^(%s)%s(%s)$`, regexChainID, regexSeparator, regexEpoch))
+	regexChainID   = `[a-z]*`
+	regexSeparator = `-{1}`
+	regexEpoch     = `[1-9][0-9]*`
+	routerChainID  = regexp.MustCompile(fmt.Sprintf(`^(%s)%s(%s)$`, regexChainID, regexSeparator, regexEpoch))
 )
 
 // IsValidChainID returns false if the given chain identifier is incorrectly formatted.
@@ -23,7 +23,7 @@ func IsValidChainID(chainID string) bool {
 		return false
 	}
 
-	return injectiveChainID.MatchString(chainID)
+	return routerChainID.MatchString(chainID)
 }
 
 // ParseChainID parses a string chain identifier's epoch to an Ethereum-compatible
@@ -34,7 +34,7 @@ func ParseChainID(chainID string) (*big.Int, error) {
 		return nil, sdkerrors.Wrapf(ErrInvalidChainID, "chain-id '%s' cannot exceed 48 chars", chainID)
 	}
 
-	matches := injectiveChainID.FindStringSubmatch(chainID)
+	matches := routerChainID.FindStringSubmatch(chainID)
 	if matches == nil || len(matches) != 3 || matches[1] == "" {
 		return nil, sdkerrors.Wrap(ErrInvalidChainID, chainID)
 	}
@@ -50,5 +50,5 @@ func ParseChainID(chainID string) (*big.Int, error) {
 
 // GenerateRandomChainID returns a random chain-id in the valid format.
 func GenerateRandomChainID() string {
-	return fmt.Sprintf("injective-%d", 10+tmrand.Intn(10000))
+	return fmt.Sprintf("router-%d", 10+tmrand.Intn(10000))
 }
