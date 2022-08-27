b4a8fc8573dd6aec9858590a508c1dfd539afba4
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2022-08-27 22:28:03 +0400

    Msg set orchestrator address

---
+++ examples/chain/Attestation/1_MsgSetOrchestratorAddress/example.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	chainclient "github.com/router-protocol/sdk-go/client/chain"
+	"github.com/router-protocol/sdk-go/client/common"
+
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
+
+	attestationTypes "github.com/router-protocol/sdk-go/routerchain/attestation/types"
+)
+
+const (
+	// Tx Agrs
+	ORCHESTRATOR_ETH_ADDRESS = "0xdE23C5FfC7B045b48F0B85ADA2c518d213d9e24F"
+)
+
+func main() {
+	network := common.LoadNetwork("local", "k8s")
+	tmRPC, err := rpchttp.New(network.TmEndpoint, "/websocket")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Network", network)
+	senderAddress, cosmosKeyring, err := chainclient.InitCosmosKeyring(
+		os.Getenv("HOME")+"/.routerd",
+		"routerd",
+		"file",
+		"genesis",
+		"12345678",
+		"", // keyring will be used if pk not provided
+		false,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// initialize grpc client
+	clientCtx, err := chainclient.NewClientContext(
+		network.ChainId,
+		senderAddress.String(),
+		cosmosKeyring,
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	clientCtx = clientCtx.WithNodeURI(network.TmEndpoint).WithClient(tmRPC)
+
+	// prepare tx msg
+	val := sdktypes.ValAddress(clientCtx.GetFromAddress())
+	msg := attestationTypes.NewMsgSetOrchestratorAddress(val.String(), senderAddress.String(), ORCHESTRATOR_ETH_ADDRESS)
+
+	chainClient, err := chainclient.NewChainClient(
+		clientCtx,
+		network.ChainGrpcEndpoint,
+		// common.OptionTLSCert(network.ChainTlsCert),
+		common.OptionGasPrices("100000000000000router"),
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//AsyncBroadcastMsg, SyncBroadcastMsg, QueueBroadcastMsg
+	err = chainClient.QueueBroadcastMsg(msg)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	time.Sleep(time.Second * 5)
+
+	gasFee, err := chainClient.GetGasFee()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("gas fee:", gasFee)
+}
