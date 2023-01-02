369e2f2d253bbd5d37dad0c4f41d5363bc91a835
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2023-01-02 14:23:18 +0530

    signature filtering

--- examples/lightweight-relayer/main.go
+++ examples/lightweight-relayer/main.go
@@ -13,19 +13,20 @@ import (
 
 const (
 	// Tx Agrs
-	NETWORK_NAME              = "devnet-internal"
+	NETWORK_NAME              = "devnet"
 	ORCHESTRATOR_KEYRING_FROM = "orchestrator"
 	PASSPHRASE                = "12345678"
 
 	RELAYER_PRIVATE_KEY    = "982E38580B1D24D1C7DE91DE112F89F200E09E31395837DFBAF5A62D9FBE44F7"
 	RELAYER_ETH_ADDRESS    = "0x33B4A007EcC80Bc99578c18Da07da704c5403236"
 	RELAYER_ROUTER_ADDRESS = "router1fcvd3vcer6h5n37cqtajgwy6uk32w0t0wptqg9"
-	ETH_RPC                = "https://rpc.ankr.com/avalanche_fuji"
-	// ETH_RPC                = "https://polygon-mumbai.g.alchemy.com/v2/7PDGHtfRIuSxeCJwzBlDLqk_6I20AIvz"
+	// ETH_RPC                = "https://rc-testnet1.routerprotocol.com"
+	// ETH_RPC = "https://polygon-mumbai.g.alchemy.com/v2/7PDGHtfRIuSxeCJwzBlDLqk_6I20AIvz"
+	ETH_RPC = "https://rpc.ankr.com/avalanche_fuji"
 )
 
 var (
-	GATEWAY_CONTRACT_ADDRESS = ethcmn.HexToAddress("0x125923830dEDd7F8F40ec3F9b9c51C8236b02440")
+	GATEWAY_CONTRACT_ADDRESS = ethcmn.HexToAddress("0xEF68E2410652413D1d011423B08F6e8A6c2D8B86")
 )
 
 func main() {
