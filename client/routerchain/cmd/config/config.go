4778c550615dbc9ef4dfc0391a7fa0f13d6bb7d1
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2023-03-09 15:48:53 +0400

    set prefix

---
+++ client/routerchain/cmd/config/config.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	ethaccounts "github.com/ethereum/go-ethereum/accounts"
+)
+
+const (
+	// RouterEvmBech32Prefix defines the Bech32 prefix used for EthAccounts on the Router Chain
+	RouterEvmBech32Prefix = "router"
+
+	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
+	Bech32PrefixAccAddr = RouterEvmBech32Prefix
+	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
+	Bech32PrefixAccPub = RouterEvmBech32Prefix + sdk.PrefixPublic
+	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
+	Bech32PrefixValAddr = RouterEvmBech32Prefix + sdk.PrefixValidator + sdk.PrefixOperator
+	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
+	Bech32PrefixValPub = RouterEvmBech32Prefix + sdk.PrefixValidator + sdk.PrefixOperator + sdk.PrefixPublic
+	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
+	Bech32PrefixConsAddr = RouterEvmBech32Prefix + sdk.PrefixValidator + sdk.PrefixConsensus
+	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
+	Bech32PrefixConsPub = RouterEvmBech32Prefix + sdk.PrefixValidator + sdk.PrefixConsensus + sdk.PrefixPublic
+
+	// Bip44CoinType satisfies EIP84. See https://github.com/ethereum/EIPs/issues/84 for more info.
+	Bip44CoinType = 60
+)
+
+const (
+	// DisplayDenom defines the denomination displayed to users in client applications.
+	DisplayDenom = "route"
+)
+
+var (
+	// BIP44HDPath is the BIP44 HD path used on Ethereum.
+	BIP44HDPath = ethaccounts.DefaultBaseDerivationPath.String()
+)
+
+// SetBech32Prefixes sets the global prefixes to be used when serializing addresses and public keys to Bech32 strings.
+func SetBech32Prefixes(config *sdk.Config) {
+	config.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
+	config.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
+	config.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
+}
+
+// SetBip44CoinType sets the global coin type to be used in hierarchical deterministic wallets.
+func SetBip44CoinType(config *sdk.Config) {
+	config.SetCoinType(Bip44CoinType)
+	config.SetFullFundraiserPath(BIP44HDPath)
+}
+
+// RegisterDenoms registers the base and display denominations to the SDK.
+func RegisterDenoms() {
+	if err := sdk.RegisterDenom(DisplayDenom, sdk.OneDec()); err != nil {
+		panic(err)
+	}
+
+	// if err := sdk.RegisterDenom(chaintypes.RouterCoin, sdk.NewDecWithPrec(1, chaintypes.BaseDenomUnit)); err != nil {
+	// 	panic(err)
+	// }
+}
