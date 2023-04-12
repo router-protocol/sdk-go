9df34b491b18adf309caa6cc1d9fb3ce59fbfa0c
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2023-04-12 22:21:58 +0400

    Crosschain ack confirm workflow

---
+++ examples/routerchain/Crosschain/QueryCrosschainAckConfirmations/example.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	chainclient "github.com/router-protocol/sdk-go/client/routerchain"
+	"github.com/router-protocol/sdk-go/client/routerchain/common"
+
+	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
+)
+
+const (
+	// Tx Agrs
+	ORCHESTRATOR_ETH_ADDRESS = "0x46e551558388e670ac58e6C84c0759Ca2dCBe6e3"
+)
+
+func main() {
+	network := common.LoadNetwork("local", "k8s")
+	tmRPC, err := rpchttp.New(network.TmEndpoint, "/websocket")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Network", network)
+	senderAddress, cosmosKeyring, err := chainclient.InitCosmosKeyring(
+		os.Getenv("HOME")+"/.routerd",
+		"routerd",
+		"file",
+		"orchestrator",
+		"12345678",
+		"", // keyring will be used if pk not provided
+		false,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// initialize grpc client
+	clientCtx, err := chainclient.NewClientContext(
+		network.ChainId,
+		senderAddress.String(),
+		cosmosKeyring,
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	clientCtx = clientCtx.WithNodeURI(network.TmEndpoint).WithClient(tmRPC)
+
+	chainClient, err := chainclient.NewChainClient(
+		clientCtx,
+		network.ChainGrpcEndpoint,
+		// common.OptionTLSCert(network.ChainTlsCert),
+		common.OptionGasPrices("100000000000000route"),
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), time.Minute)
+	allCrosschainAckRequests, err := chainClient.GetAllCrosschainAckRequests(ctx, nil)
+	fmt.Println("allCrosschainAckRequests", allCrosschainAckRequests, "error", err)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, crosschainAckRequest := range allCrosschainAckRequests.CrosschainAckRequest {
+		claimhash, _ := crosschainAckRequest.ClaimHash()
+		allCrosschainAckRequestConfirmations, err := chainClient.GetAllCrosschainRequestAckConfirmations(ctx, nil, crosschainAckRequest.ChainId, crosschainAckRequest.EventNonce, claimhash)
+		fmt.Println("allCrosschainAckRequestConfirmations", allCrosschainAckRequestConfirmations, "error", err)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	time.Sleep(time.Second * 5)
+
+	gasFee, err := chainClient.GetGasFee()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("gas fee:", gasFee)
+}
