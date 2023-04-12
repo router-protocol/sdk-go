6b7d7699b236ecc15ef0d5bfed546fb1c0cf959e
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2023-04-12 21:47:58 +0400

    Crosschain ack confirm workflow

--- routerchain/crosschain/types/message_crosschain_ack_request.go
+++ routerchain/crosschain/types/message_crosschain_ack_request.go
@@ -10,7 +10,7 @@ const TypeMsgCrosschainAckRequest = "crosschain_ack_request"
 
 var _ sdk.Msg = &MsgCrosschainAckRequest{}
 
-func NewMsgCrosschainAckRequest(orchestrator string, chainId string, eventNonce uint64, blockHeight uint64, destTxHash string, relayerRouterAddress string, sourceChainId string, requestIdentifier uint64, execData []byte, execStatus bool, ethSigner string, signature string) *MsgCrosschainAckRequest {
+func NewMsgCrosschainAckRequest(orchestrator string, chainId string, eventNonce uint64, blockHeight uint64, destTxHash string, relayerRouterAddress string, sourceChainId string, requestIdentifier uint64, execData []byte, execStatus bool) *MsgCrosschainAckRequest {
 	return &MsgCrosschainAckRequest{
 		Orchestrator:         orchestrator,
 		ChainId:              chainId,
@@ -22,8 +22,6 @@ func NewMsgCrosschainAckRequest(orchestrator string, chainId string, eventNonce 
 		RequestIdentifier:    requestIdentifier,
 		ExecData:             execData,
 		ExecStatus:           execStatus,
-		EthSigner:            ethSigner,
-		Signature:            signature,
 	}
 }
 
