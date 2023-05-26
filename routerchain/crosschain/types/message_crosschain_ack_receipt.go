0ba773b578e86857e40da24108084b508e09395c
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2023-05-27 00:31:58 +0400

    fix ack receipt exec status

--- routerchain/crosschain/types/message_crosschain_ack_receipt.go
+++ routerchain/crosschain/types/message_crosschain_ack_receipt.go
@@ -12,7 +12,7 @@ const TypeMsgCrosschainAckReceipt = "crosschain_ack_receipt"
 
 var _ sdk.Msg = &MsgCrosschainAckReceipt{}
 
-func NewMsgCrosschainAckReceipt(orchestrator string, ackReceiptSrcChainId string, ackReceiptIdentifier uint64, ackReceiptBlockHeight uint64, ackReceiptTxHash string, relayerRouterAddress string, requestIdentifier uint64, ackSrcChainId string, ackRequestIdentifier uint64, feeConsumed uint64) *MsgCrosschainAckReceipt {
+func NewMsgCrosschainAckReceipt(orchestrator string, ackReceiptSrcChainId string, ackReceiptIdentifier uint64, ackReceiptBlockHeight uint64, ackReceiptTxHash string, relayerRouterAddress string, requestIdentifier uint64, ackSrcChainId string, ackRequestIdentifier uint64, feeConsumed uint64, ackExecData []byte, ackExecStatus bool) *MsgCrosschainAckReceipt {
 	return &MsgCrosschainAckReceipt{
 		Orchestrator:          orchestrator,
 		AckReceiptSrcChainId:  ackReceiptSrcChainId,
@@ -24,6 +24,8 @@ func NewMsgCrosschainAckReceipt(orchestrator string, ackReceiptSrcChainId string
 		AckSrcChainId:         ackSrcChainId,
 		AckRequestIdentifier:  ackRequestIdentifier,
 		FeeConsumed:           feeConsumed,
+		AckExecData:           ackExecData,
+		AckExecStatus:         ackExecStatus,
 	}
 }
 
