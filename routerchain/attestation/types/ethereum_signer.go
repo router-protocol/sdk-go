15627e6c313daeee49f1dbe7a84347de49387f97
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2024-02-20 16:57:58 +0400

    add bitcoin chain type

--- routerchain/attestation/types/ethereum_signer.go
+++ routerchain/attestation/types/ethereum_signer.go
@@ -57,8 +57,54 @@ func EthAddressFromSignature(hash []byte, signature []byte) (*EthAddress, error)
 	return addr, nil
 }
 
-// ValidateEthereumSignature takes a message, an associated signature and public key and
-// returns an error if the signature isn't valid
+func EthAddressFromSignatureStarknet(hash []byte, signature []byte) (*EthAddress, error) {
+	if len(signature) < 65 {
+		return nil, sdkerrors.Wrap(ErrInvalid, "signature too short")
+	}
+	// To verify signature
+	// - use crypto.SigToPub to get the public key
+	// - use crypto.PubkeyToAddress to get the address
+	// - compare this to the address given.
+
+	// for backwards compatibility reasons  the V value of an Ethereum sig is presented
+	// as 27 or 28, internally though it should be a 0-3 value due to changed formats.
+	// It seems that go-ethereum expects this to be done before sigs actually reach it's
+	// internal validation functions. In order to comply with this requirement we check
+	// the sig an dif it's in standard format we correct it. If it's in go-ethereum's expected
+	// format already we make no changes.
+	//
+	// We could attempt to break or otherwise exit early on obviously invalid values for this
+	// byte, but that's a task best left to go-ethereum
+	if signature[64] == 27 || signature[64] == 28 {
+		signature[64] -= 27
+	}
+
+	pubkey, err := crypto.SigToPub(hash, signature)
+	if err != nil {
+		return nil, sdkerrors.Wrap(err, "signature to public key")
+	}
+
+	addr, err := NewEthAddress(crypto.PubkeyToAddress(*pubkey).Hex())
+	if err != nil {
+		return nil, sdkerrors.Wrap(err, "invalid address from public key")
+	}
+
+	return addr, nil
+}
+
+func ValidateEthereumSignatureStarknet(hash []byte, signature []byte, ethAddress EthAddress) error {
+	addr, err := EthAddressFromSignatureStarknet(hash, signature)
+	if err != nil {
+		return sdkerrors.Wrap(err, "unable to get address from signature")
+	}
+
+	if addr.GetAddress() != ethAddress.GetAddress() {
+		return sdkerrors.Wrap(ErrInvalid, "signature not matching")
+	}
+
+	return nil
+}
+
 func ValidateEthereumSignature(hash []byte, signature []byte, ethAddress EthAddress) error {
 	addr, err := EthAddressFromSignature(hash, signature)
 	if err != nil {
@@ -70,4 +116,17 @@ func ValidateEthereumSignature(hash []byte, signature []byte, ethAddress EthAddr
 	}
 
 	return nil
+}
+
+func ValidateSignatureWithFallback(hash []byte, signature []byte, ethAddress EthAddress) error {
+	err := ValidateEthereumSignature(hash, signature, ethAddress)
+	if err == nil {
+		return nil
+	}
+
+	errStarknet := ValidateEthereumSignatureStarknet(hash, signature, ethAddress)
+	if errStarknet == nil {
+		return nil
+	}
+	return errStarknet
 }
