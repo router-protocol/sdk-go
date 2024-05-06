c445a39f71c982f24be9cb6bb0fd849bc6fe9ee1
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2024-05-06 14:10:42 +0400

    Alephzero checkpoint changes

--- routerchain/crosschain/types/ack_checkpoint.go
+++ routerchain/crosschain/types/ack_checkpoint.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"encoding/hex"
 	"math/big"
 	"strings"
 
@@ -39,11 +40,69 @@ func (msg CrosschainAckRequest) GetCheckpoint(routerIDstring string) ([]byte, er
 	switch msg.AckDestChainType {
 	case multichainTypes.CHAIN_TYPE_NEAR, multichainTypes.CHAIN_TYPE_POLKADOT, multichainTypes.CHAIN_TYPE_BITCOIN:
 		return msg.GetNearCheckpoint("")
+	case multichainTypes.CHAIN_TYPE_ALEPH_ZERO:
+		return msg.GetAlephZeroCheckpoint("")
 	case multichainTypes.CHAIN_TYPE_COSMOS:
 		return nil, nil
 	default:
 		return msg.GetEvmCheckpoint("")
 	}
+}
+
+func (msg CrosschainAckRequest) GetAlephZeroCheckpoint(routerIDstring string) ([]byte, error) {
+	//////////////////////////////////////////////////////////////////////
+	/////  Build data with types required for iAck gateway call  /////
+	//////////////////////////////////////////////////////////////////////
+
+	methodNameBytes := []uint8("iAck")
+	var crosschainAckMethodName [32]uint8
+	copy(crosschainAckMethodName[:], methodNameBytes)
+
+	requestIdentifier := &big.Int{}
+	requestIdentifier.SetUint64(msg.RequestIdentifier)
+
+	ackRequestIdentifier := &big.Int{}
+	ackRequestIdentifier.SetUint64(msg.AckRequestIdentifier)
+
+	var requestSender [32]uint8
+	requestSenderByte, err := hex.DecodeString(msg.RequestSender)
+	if err != nil {
+		return nil, err
+	}
+	copy(requestSender[:], requestSenderByte)
+
+	/////////////////////////////////////////////////
+	/////  pack abi for iReceive function  //////////
+	/////////////////////////////////////////////////
+	abiDef, err := abi.JSON(strings.NewReader(util.CrosschainAckRequestAlephZeroCheckpointABIJSON))
+	if err != nil {
+		return nil, err
+	}
+
+	// the methodName needs to be the same as the 'name' above in the checkpointAbiJson
+	// but other than that it's a constant that has no impact on the output. This is because
+	// it gets encoded as a function name which we must then discard.
+	abiEncodedBatch, err := abiDef.Pack("checkpoint",
+		crosschainAckMethodName,
+		msg.AckDestChainId,
+		requestIdentifier,
+		ackRequestIdentifier,
+		msg.AckSrcChainId,
+		requestSender,
+		msg.ExecData,
+		msg.ExecStatus,
+	)
+
+	// this should never happen outside of test since any case that could crash on encoding
+	// should be filtered above.
+	if err != nil {
+		return nil, err
+	}
+
+	// we hash the resulting encoded bytes discarding the first 4 bytes these 4 bytes are the constant
+	// method name 'checkpoint'. If you were to replace the checkpoint constant in this code you would
+	// then need to adjust how many bytes you truncate off the front to get the output of abi.encode()
+	return crypto.Keccak256Hash(abiEncodedBatch[4:]).Bytes(), nil
 }
 
 func (msg CrosschainAckRequest) GetNearCheckpoint(routerIDstring string) ([]byte, error) {
